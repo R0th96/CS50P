fd9cbed89bafefe19ab18a236e98e6802a3e2740
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-05-09 23:24:55 +0700

    Add files via upload

---
+++ 001hello.py
@@ -0,0 +1,21 @@
+#Purpose of comments
+#Pseudocode or outlines for your program
+
+#Ways to use comments
+#Here is the comments
+"""
+Here is the comments
+"""
+
+#Say hello to user
+print("Hello Dear User.")
+
+#Asking for name
+#input function will convert the word you type into string
+name = input("What is your name, Sir?")
+print("Then, Hello Dear ", name)
+
+#Say thanks you
+#two ways to use print multiple arguments
+print("Thanks for using our service, " + name)
+print("Thanks for using our service", name)
