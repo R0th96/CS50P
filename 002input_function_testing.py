088e2eccfd6b8f4b74e18d01e27dd8e6c27b71ca
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-05-12 23:17:00 +0700

    Delete 002input_function_testing.py

--- 002input_function_testing.py
+++
@@ -1,9 +0,0 @@
-#input function will convert the word you type into string
-#number1 = input("enter number ")
-#above input is not working as the input value is converted to str
-number1 = 40
-number2 = 3
-total = number1 + number2
-
-print(total)
-
