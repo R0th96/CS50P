22bcc7f3b2812bac2a61c490a665c19f01c66be6
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-05-21 14:22:06 +0700

    Add files via upload

---
+++ Week03/0301Test.py
@@ -0,0 +1,15 @@
+#What are the types of error you know with example? 
+#Syntax Error
+print("Hello World")
+#if you forget to add one bracket or one comma
+
+#Value Error
+x = int(input("Input something int "))
+print(x)
+#if the input value is not digit but alpha
+
+#Name Error
+y = 2
+print (y)
+#before using variable y
+#if you forget to clarify variable y or variable y can't get the user input which leads to no value in variable y
