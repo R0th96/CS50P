eff2c5310da1882b080ab41aa6e61afe620a5216
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-21 04:00:39 +0700

    A program to record the order and print out the order is created

--- Own_Test/OT003Test.py
+++ Own_Test/OT003Test.py
@@ -1,6 +1,26 @@
-print("I")
-print("Love")
-print("Akari")
+def main():
+    total_type = []
+    total_cost = []
+    while True:
+        try:
+            type, cost = usage()
+            total_type += [type]
+            total_cost += [cost]
+        except EOFError:
+            print()
+            for i in range(len(total_type)):
+                print(total_type[i], total_cost[i])
+            print("Total is", sum(total_cost), "Baht")
+            break
+def usage():
+    try:
+        type = input("Where did you spend? ")
+        cost = float(input("How much? "))
+        return type, cost
+    except ValueError:
+        pass
+
+main()
 
 
 """
