f2deffd5fe3c6f59f5209074063f4da70318174b
R0th2 <froopp2@gmail.com>
2024-06-20 17:31:39 +0700

    Adding comments

--- Own_Test/OT003Test.py
+++ Own_Test/OT003Test.py
@@ -2,3 +2,13 @@ a = 4
 b = 4
 print(a, b, "Akari")
 print("Hello World")
+
+"""
+git --version
+git clone
+git add file
+git commit
+git push
+git remote set-url origin git@github.com:<Username>/<Project>.git
+
+"""
