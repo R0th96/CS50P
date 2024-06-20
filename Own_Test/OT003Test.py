93f222fe752eabdd3d53f717942a9ac03e8a450f
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-21 03:12:57 +0700

    Add some text

--- Own_Test/OT003Test.py
+++ Own_Test/OT003Test.py
@@ -1,14 +1,17 @@
-a = 4
-b = 4
-print(a, b, "Akari")
-print("Hello World")
+print("I")
+print("Love")
+print("Akari")
+
 
 """
 git --version
+git config --global user.name NAME
+git config --global user.email MAIL
 git clone
 git add file
 git commit
 git push
 git remote set-url origin git@github.com:<Username>/<Project>.git
+
 
 """
