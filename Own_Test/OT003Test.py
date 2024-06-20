04c2c21fd30de61c3c8d26bdc263780661a143f0
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-21 05:02:26 +0700

    Delete comments

--- Own_Test/OT003Test.py
+++ Own_Test/OT003Test.py
@@ -24,15 +24,4 @@ main()
 print(f"I Love Akari"*5)
 
 
-"""
-git --version
-git config --global user.name NAME
-git config --global user.email MAIL
-git clone
-git add file
-git commit
-git push
-git remote set-url origin git@github.com:<Username>/<Project>.git
 
-
-"""
