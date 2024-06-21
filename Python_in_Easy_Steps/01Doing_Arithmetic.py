fb7c1db77fdd6269bc895d0f109c4953b0a33afb
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-21 12:37:53 +0700

    Added Arithmetic functions

---
+++ Python_in_Easy_Steps/01Doing_Arithmetic.py
@@ -0,0 +1,19 @@
+"""
+Addition
+Subtraction
+Multiplication
+Division
+modulo
+Floor Division
+Exponent
+"""
+
+a = 8
+b = 2
+print("Addition", a + b)
+print("Subtraction", a - b)
+print("Multiplication", a * b)
+print("Division", a / b)
+print("modulo", a % b)
+print("Floor Division", a // b)
+print("Exponent", a ** b)
