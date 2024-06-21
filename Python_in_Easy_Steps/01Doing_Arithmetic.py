d20b3fd9de131f1a2206ef8258c6a7a56cabf85e
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-21 13:30:40 +0700

    Added operation and assigning example

--- Python_in_Easy_Steps/01Doing_Arithmetic.py
+++ Python_in_Easy_Steps/01Doing_Arithmetic.py
@@ -17,3 +17,32 @@ print("Division", a / b)
 print("modulo", a % b)
 print("Floor Division", a // b)
 print("Exponent", a ** b)
+print("\n")
+
+#Short hand form for operation
+a, b = (8, 2)
+a += b
+print("Addition & Assign", a) 
+a, b = (8, 2)
+a -= b
+print("Subtraction & Assign", a)
+a, b = (8, 2)
+a *= b
+print("Multiplication & Assign", a)
+a, b = (8, 2)
+a /= b
+print("Division & Assign", a)
+a, b = (8, 2)
+a %= b
+print("modulo & Assign", a)
+a, b = (8, 2)
+a //= b
+print("Floor Division & Assign", a)
+a, b = (8, 2)
+a **= b
+print("Exponent & Assign", a)
+print("\n")
+
+print("Name:\t", "Htet Wai Yan Toe")
+print("GF:\t", "Akari Min Thant")
+
