4754a922a725c815b4d6e41d6691f1102c8e1a9a
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-06-22 22:28:55 +0700

    Finalized Version of Unit Test example 1

--- Week05/Test_Calculator.py
+++ Week05/Test_Calculator.py
@@ -16,10 +16,15 @@ if __name__ == "__main__":
     main()
 """
 
-def test_square():
+#It is better to divide the test into multiple tests
+def test_positive():
     assert(square(2) == 4)
     assert(square(3) == 9)
+
+def test_zero():
     assert(square(0) == 0)
+    
+def test_negative():
     assert(square(-2) == 4)
-    assert(square(-2) == 9)
+    assert(square(-3) == 9)
     
